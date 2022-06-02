e01547fef1ef41bdb0eaa432e4ec229491206546
Fuyukiri <ns34gp@hotmail.com>
2022-06-02 00:43:06 -0700

    add 2d array

--- loop_play/src/main.rs
+++ loop_play/src/main.rs
@@ -26,3 +26,34 @@ pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
     }
     v
 }
+
+// 2d array
+
+pub fn transpose(matrix: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let le1 = matrix.len();
+    let le2 = matrix[0].len();
+    let mut new_arr = vec![vec![0; le1]; le2];
+    for i in 0..le1 {
+        for j in 0..le2 {
+            new_arr[j][i] = matrix[i][j]
+        }
+    }
+    return new_arr;
+}
+
+pub fn transpose2(matrix: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let mut res:Vec<Vec<i32>> = Vec::new();
+    for i in 0..matrix.len() {
+        
+        for j in 0..matrix[0].len() {
+            if i == 0 {
+                let mut cur:Vec<i32> = Vec::new();
+                res.push(cur);
+            }
+            
+            res[j].push(matrix[i][j]);
+        }
+        
+    }
+    res
+}
